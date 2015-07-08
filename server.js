a6f651cfd8dc431db7ac04537e4f00fad102eebf
Chris Mather <chris@eventedmind.com>
2015-07-08 16:08:58 -0700

    read content from disk files

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 var http = require('http');
+var fs = require('fs');
 
 http.createServer(function (request, response) {
   switch(request.url) {
     case '/':
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/html');
-      response.end('<html><body><h1>hello world!</h1></body></html>\n');
+      var content = fs.readFileSync('app/app.html');
+      response.end(content + '\n');
       break;
     case '/app.js':
       response.statusCode = 200;
       response.setHeader('Content-Type', 'text/javascript');
-      response.end('console.log("hello world!");\n');
+      var content = fs.readFileSync('app/app.js');
+      response.end(content + '\n');
       break;
   }
 }).listen(3000);
