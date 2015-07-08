752d881de9add365124be875b2af865d8dd4288a
Chris Mather <chris@eventedmind.com>
2015-07-08 14:55:05 -0700

    add app.js url

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 var http = require('http');
 
 http.createServer(function (request, response) {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/html');
-  response.end('<html><body><h1>hello world!</h1></body></html>\n');
+  switch(request.url) {
+    case '/':
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/html');
+      response.end('<html><body><h1>hello world!</h1></body></html>\n');
+      break;
+    case '/app.js':
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/javascript');
+      response.end('console.log("hello world!");\n');
+      break;
+  }
 }).listen(3000);
 
 console.log('http server listening on http://localhost:3000');
