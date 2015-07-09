a910897281752f9d59a3ccde4ad21168520a05fb
Chris Mather <chris@eventedmind.com>
2015-07-09 11:19:06 -0700

    add css and form submit example

--- server.js
+++ server.js
@@ -1,7 +1,27 @@
 var http = require('http');
 var fs = require('fs');
 
+function printRequest(request) {
+  console.log('*********************************************************');
+  console.log(request.method + " " + request.url);
+  console.log('*********************************************************');
+  for (var key in request.headers) {
+    console.log(key.toUpperCase() + ": " + request.headers[key]);
+  }
+
+  request.on('data', function (chunk) {
+    console.log("");
+    console.log(chunk.toString());
+  });
+
+  request.on('end', function () {
+  console.log('*********************************************************');
+  });
+}
+
 http.createServer(function (request, response) {
+  printRequest(request);
+
   switch(request.url) {
     case '/':
       response.statusCode = 200;
@@ -14,6 +34,18 @@ http.createServer(function (request, response) {
       response.setHeader('Content-Type', 'text/javascript');
       var content = fs.readFileSync('app/app.js');
       response.end(content + '\n');
+      break;
+    case '/app.css':
+      response.statusCode = 200;
+      response.setHeader('Content-Type', 'text/css');
+      var content = fs.readFileSync('app/app.css');
+      response.end(content + '\n');
+      break;
+    case '/submit':
+      response.statusCode = 302;
+      response.setHeader('Location', '/');
+      response.setHeader('Content-Type', 'text/html');
+      response.end();
       break;
   }
 }).listen(3000);
